764790ea140eeba396f6c65324163d09ab9aabba
MichalakSzymon <szymon.michalak@vp.pl>
2018-02-19 23:16:02 +0100

    testing conection between microservices

---
+++ EmployeeSerachService/src/main/java/example/EmployeeSerachService/EmployeeSerachServiceApplication.java
@@ -0,0 +1,12 @@
+package example.EmployeeSerachService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSerachServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSerachServiceApplication.class, args);
+	}
+}
